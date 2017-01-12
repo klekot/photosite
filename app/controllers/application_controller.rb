2ffb150e807a3e83c26d41c38f2ea55d77686791
Igor Klekotnev <klekotnev@gmail.com>
2017-01-12 16:39:47 +0300

    try fix bug with changing pix order5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,14 +13,26 @@ class ApplicationController < ActionController::Base
   end
 
   def update_order(id_array)
-    old_ordered_photos = []
+    old_order = []
     Photo.published.main_page.order(:order).each do |photo|
-      old_ordered_photos.push photo
+      old_order.push photo.id
     end
-    id_array.each_with_index do |id, i|
-      ph = old_ordered_photos[i]
-      ph.order = id
-      ph.save
+    Photo.all.each do |photo|
+      photo.order=nil
+      photo.save
+    end
+    id_array.each_with_index do |ida, i|
+      if old_order[ida.to_s.to_i]
+        ph = Photo.find(old_order[ida.to_s.to_i])
+        ph.order = i
+        ph.save
+      end
+    end
+    last_order_number = 0
+    last_order_number = (Photo.where.not(order: nil).order(order: :desc).first).order if Photo.where.not(order: nil).size > 0
+    Photo.where(order: nil).each_with_index do |photo, i|
+      photo.order=last_order_number + 1 + i
+      photo.save
     end
   end
 end
