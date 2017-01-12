53ee615a6699efdbd567eb8f809fe917eccdd5d0
Igor Klekotnev <klekotnev@gmail.com>
2017-01-12 18:11:08 +0300

    try fix bug with changing pix order6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,26 +13,38 @@ class ApplicationController < ActionController::Base
   end
 
   def update_order(id_array)
-    old_order = []
-    Photo.published.main_page.order(:order).each do |photo|
-      old_order.push photo.id
+    #fix old order of pix
+    id_order = []
+    id_array.each do |ida|
+      id_order.push (Photo.find_by(order: ida)).id
     end
+    #reset all order numbers of all pix
     Photo.all.each do |photo|
       photo.order=nil
       photo.save
     end
-    id_array.each_with_index do |ida, i|
-      if old_order[ida.to_s.to_i]
-        ph = Photo.find(old_order[ida.to_s.to_i])
-        ph.order = i
-        ph.save
+    if id_array.size == id_order.size
+      # id_array.each_with_index do |ida, i|
+      #   if id_order[ida.to_s.to_i]
+      #     ph = Photo.find(id_order[ida.to_s.to_i])
+      #     ph.order = i
+      #     ph.save
+      #   end
+      # end
+      id_order.each_with_index do |id, i|
+        p = Photo.find(id)
+        p.order = i
+        p.save
       end
-    end
-    last_order_number = 0
-    last_order_number = (Photo.where.not(order: nil).order(order: :desc).first).order if Photo.where.not(order: nil).size > 0
-    Photo.where(order: nil).each_with_index do |photo, i|
-      photo.order=last_order_number + 1 + i
-      photo.save
+      last_order_number = 0
+      last_order_number = (Photo.where.not(order: nil).order(order: :desc).first).order if Photo.where.not(order: nil).size > 0
+      Photo.where(order: nil).each_with_index do |photo, i|
+        photo.order=last_order_number + 1 + i
+        photo.save
+      end
+    else
+      puts "Error!!!"
+      return
     end
   end
 end
